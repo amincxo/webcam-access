c40ba3ece38702baa7f072f0a21d0b494207409c
Amin Borvayeh <75912019+amincxo@users.noreply.github.com>
2022-02-12 00:44:42 +0330

    final commit done

---
+++ script.js
@@ -0,0 +1,10 @@
+const video = document.getElementById ('video')
+
+function startVideo() {
+    navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.log(err)
+    )
+}
+startVideo()
